ad1a2d8b0b577f2e7c344dc231ce379a1f59ca88
Artem <art_nazarov@mail.ru>
2019-07-02 20:19:43 +0300

    Добавил спиннер

--- app/src/main/java/education/artem/contrastchangeandroid/fragments/ContourFragment.java
+++ app/src/main/java/education/artem/contrastchangeandroid/fragments/ContourFragment.java
@@ -1,26 +1,37 @@
 package education.artem.contrastchangeandroid.fragments;
 
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-import java.util.Objects;
-
-import education.artem.contrastchangeandroid.MainActivity;
 import education.artem.contrastchangeandroid.R;
 
 public class ContourFragment extends Fragment {
+
+
+
     @NonNull
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
+        Spinner spinner = container.findViewById(R.id.contoursSpinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.contours_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
         return inflater.inflate(
                 R.layout.contour_parameters, container, false);
     }
+
 
 }
